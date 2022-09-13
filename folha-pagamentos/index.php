a71e57f00693cfe82b2f937e439381a829f8d81a
Vinícius <80614001+viniscrv@users.noreply.github.com>
2022-09-13 14:22:59 -0300

    atualizando titulos

--- folha-pagamentos/index.php
+++ folha-pagamentos/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Folha de pagamentos</title>
+    <title>Folha de Pagamentos</title>
     <style type="text/css">
         * {
         margin: 0;
